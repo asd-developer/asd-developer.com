0fc1c595840f0f018b229ed3608d859722c67253
Andre Duarte <andreduarte@MacBook-de-Andre.local>
2020-09-30 01:37:49 +0100

    Fixing issues with font not loading on mobile 6

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -3,9 +3,6 @@ import { createGlobalStyle } from "styled-components"
 export const GlobalStyles = createGlobalStyle`
   body {
     background-color: #EAF2F6;
-    @font-face {
-      font-family: 'Lexend Deca', sans-serif;
-      src: url('https://fonts.googleapis.com/css2?family=Lexend+Deca&display=swap');
-    }
+    font-family: 'LexendDeca-Regular';
   }
 `
