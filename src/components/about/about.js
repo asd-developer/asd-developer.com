b865ebf9190d92bac5722f32c785e567b8b3e49b
asd-developer <andreduarte.profissional@gmail.com>
2020-12-13 14:53:25 +0000

    Changed about me section

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -8,8 +8,7 @@ const About = () => {
             <h1>About Me</h1>
             <p>
               <br/>
-            I started having an interest in technology when I was young, I wanted to know how to use the tools that were available to me in the best way.<br/>
-            Now, I want to help make the tools that will be used by many, and shape the future I was born in by creating responsive websites with both good looks and functionality.<br/>
+              I consider myself a life long learner who is passionate about the latest front-end technologies. I love solving problems with code and creating responsive, professional-looking websites. I am also interested in cloud technologies and serverless applications. <br/>
             <br/>I have always been a visual person, I started by getting interested in UI/UX and that leads me to my first experience with code.<br/>
                 <br/>I wrote my first line of JavaScript and got addicted ever since then I started doing project after project and I am now looking for opportunities to improve my skills as a Front-end Developer.<br/>
                 <br/>I’m a professional hobbyist (I like mostly to skateboard, spearfish, travel, and pilot racing drones).
